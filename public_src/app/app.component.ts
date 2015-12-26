f7b46082f97490e7866aeea41f8f9f7ed22bf6e6
haoliangyu <njkon@foxmail.com>
2015-12-25 20:11:14 -0500

    add base map

--- public_src/app/app.component.ts
+++ public_src/app/app.component.ts
@@ -1,10 +1,31 @@
+/// <reference path="../../typings/leaflet/leaflet.d.ts"/>
+
 import {Component, View} from 'angular2/core';
+import {NavigatorComponent} from '../components/navigator/navigator.component'
 
 @Component({
-    selector: 'app',
+    selector: 'app'
 })
 @View({
     templateUrl: './app/app.component.html',
-    styleUrls: ['./app/app.component.css']
+    styleUrls: ['./app/app.component.css'],
+    directives: [NavigatorComponent]
 })
-export class AppComponent { }
+export class AppComponent {
+    constructor() {
+        var map = new L.Map('map', {
+            zoomControl: false,
+            center: new L.LatLng(40.731253, -73.996139),
+            zoom: 12,
+            minZoom: 4,
+            maxZoom: 19
+        });
+        var baseMap = new L.TileLayer("http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
+        }).addTo(map);
+        var zoomControl = L.control.zoom({
+            position: 'topright'
+        }).addTo(map);
+    }
+
+}
---
+++ public_src/components/navigator/navigator.component.ts
@@ -0,0 +1,15 @@
+import {Component, View} from 'angular2/core';
+
+@Component({
+    selector: 'navigator'
+})
+@View({
+    templateUrl: './components/navigator/navigator.component.html',
+    styleUrls: [
+        './components/navigator/navigator.component.css',
+        './styles/main.css'
+    ]
+})
+export class NavigatorComponent {
+
+}
