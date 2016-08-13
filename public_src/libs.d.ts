d158f7ade1d161739fc67692ab3162b0ae10ef72
haoliangyu <njkon@foxmail.com>
2016-08-12 22:04:14 -0400

    fix #9

---
+++ public_src/libs.d.ts
@@ -0,0 +1,7 @@
+declare module 'zone.js/dist/zone' {
+    export var Zone; // this doesn't actually do anything just makes the compiler not complain about the empty module
+};
+
+declare module 'zone.js/dist/long-stack-trace-zone' {
+    export var Zone; // this doesn't actually do anything just makes the compiler not complain about the empty module
+};
--- public_src/libs.ts
+++ public_src/libs.ts
@@ -1,7 +1,8 @@
 /// <reference path="./typings/require.d.ts"/>
 
 import 'leaflet';
-import 'zone.js';
+import 'zone.js/dist/zone';
+import 'zone.js/dist/long-stack-trace-zone';
 import 'reflect-metadata';
 
 require('../node_modules/bootstrap/dist/css/bootstrap.css');
