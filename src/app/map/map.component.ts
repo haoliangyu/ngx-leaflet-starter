33d0d7024e09789b1b38f9cfc5aa9024e42c10fa
haoliangyu <njkon@foxmail.com>
2018-02-10 18:52:52 -0500

    better way to initialize map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from "@angular/core";
+import Rx = require("rxjs/Rx");
+import * as L from "leaflet";
 import { GeocodingService } from "../geocoding.service";
 import { MapService } from "../map.service";
-import * as L from "leaflet";
+import { Location } from "../location";
+
+import "rxjs/add/operator/catch";
 
 @Component({
   selector: "app-map",
@@ -15,25 +19,33 @@ export class MapComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    const map = L.map("map", {
-      zoomControl: false,
-      center: L.latLng(40.731253, -73.996139),
-      zoom: 12,
-      minZoom: 4,
-      maxZoom: 19,
-      layers: [this.mapService.baseMaps.OpenStreetMap]
-    });
-
-    L.control.zoom({ position: "topright" }).addTo(map);
-    L.control.layers(this.mapService.baseMaps).addTo(map);
-    L.control.scale().addTo(map);
-
-    this.mapService.map = map;
     this.geocoder
       .getClientLocation()
-      .subscribe(
-        location => map.panTo(location.latlng),
-        err => console.error(err)
-      );
+      .catch(err => {
+        console.error(err);
+
+        // default map location
+        const location = new Location();
+        location.address = "New York City, New York, United States";
+        location.latlng = L.latLng(40.731253, -73.996139);
+
+        return Rx.Observable.of(location);
+      })
+      .subscribe((location: Location) => {
+        const map = L.map("map", {
+          zoomControl: false,
+          center: location.latlng,
+          zoom: 12,
+          minZoom: 4,
+          maxZoom: 19,
+          layers: [this.mapService.baseMaps.OpenStreetMap]
+        });
+
+        L.control.zoom({ position: "topright" }).addTo(map);
+        L.control.layers(this.mapService.baseMaps).addTo(map);
+        L.control.scale().addTo(map);
+
+        this.mapService.map = map;
+      });
   }
 }
